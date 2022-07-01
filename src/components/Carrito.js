7fa484c7c759c4f1f4f4fb3aca4ccfd4ff02a409
Estuar Horna <bhornaf93@gmail.com>
2022-06-30 20:45:07 -0500

    AppSabina + Brayyan Horna Fernandez

--- src/components/Carrito.js
+++ src/components/Carrito.js
@@ -1,11 +1,67 @@
+import { useContext } from "react";
 import { Link } from "react-router-dom";
+import { context } from "../CartContext";
 
-const Carrito = (props) => {
+const Carrito = () => {
+    const { cart, removeItem, clearCart } = useContext(context);
+
     return (
         <>
-            <div className="carrito">
-                <h1>Carrito</h1>
-                <Link to="/checkout">Proceder con la compra</Link>
+            <h1>Carrito</h1>
+            <div className="cart container">
+                {cart.length > 0 ? (
+                    <>
+                        <h2>{cart.length} productos</h2>
+                        <ul className="list-group">
+                            {cart.map((item) => (
+                                <li
+                                    key={item.id}
+                                    className="list-group-item d-flex justify-content-between align-items-center"
+                                >
+                                    <span>{item.title}</span>
+                                    <span>${item.price}</span>
+                                    <span> {item.quantity} </span>
+                                    <Link
+                                        to={`/Item/${item.id}`}
+                                        className="btn btn-primary"
+                                    >
+                                        Ver
+                                    </Link>
+
+                                    <button
+                                        className="btn btn-danger"
+                                        onClick={() => removeItem(item.id)}
+                                    >
+                                        Eliminar
+                                    </button>
+                                </li>
+                            ))}
+                        </ul>
+                        <div className="d-flex justify-content-between align-items-center">
+                            <span>
+                                Total: $
+                                {cart.reduce((total, item) => {
+                                    return total + item.price * item.quantity;
+                                }, 0)}
+                            </span>
+                            <button
+                                className="btn btn-danger"
+                                onClick={() => clearCart()}
+                            >
+                                Vaciar carrito
+                            </button>
+                        </div>
+                    </>
+                ) : (
+                    <>
+                        <h2>No hay productos en el carrito</h2>
+                        <Link to="/">
+                            <button className="btn btn-primary">
+                                Volver a la tienda
+                            </button>
+                        </Link>
+                    </>
+                )}
             </div>
         </>
     );
